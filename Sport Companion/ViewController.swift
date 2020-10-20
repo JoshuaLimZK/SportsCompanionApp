9258d60872b144ebeb33e83b98621c1c78de9794
joshualimzk <joshua_lim_zhe_kai@s2019.ssts.edu.sg>
2020-10-20 12:12:15 +0800

    Initial Commit

---
+++ Sport Companion/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sport Companion
+//
+//  Created by Joshua Lim on 20/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
